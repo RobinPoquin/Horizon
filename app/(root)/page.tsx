dfb52c9c650b3ea598a2d04dedc0cc8d2a441be7
unknown <robpoquin@gmail.com>
2024-08-10 15:57:22 +0200

    Création des SideBar droite et gauche et des routes correspondantes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react';
 
 const Home = () => {
   // Déclaration d'un objet loggedIn avec une propriété firstName
-  const loggedIn = { firstName: 'Robin'};
+  const loggedIn = { firstName: 'Robin', lastName: 'Poquin', email: 'robpoquin@gmail.com'};
 
   return (
     <section className='home'>
@@ -18,11 +19,18 @@ const Home = () => {
           />
           <TotalBalanceBox
             accounts={[]}
-            totalBanks={1}
+            totalBanks={3}
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50}, {currentBalance: 500.50}]}
+      />
     </section>
   );
 };
